c96ca0f5d393daa158d0ac0f0fcb4c14a4827071
Vladimir Ch <chernyshov629@gmail.com>
2021-08-03 03:42:55 +0300

    fixed minor typos in the code

--- src/Logger.php
+++ src/Logger.php
@@ -10,10 +10,11 @@ class Logger implements ILogger
 {
     /**
      * @param string $message
+     * @param string $level
      */
     public static function log(string $message, string $level = 'log'): void
     {
-        $dirName = dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR;
+        $dirName = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR;
         if (!is_dir($dirName)) mkdir($dirName, 0777, true);
         $path = $dirName . "$level.txt";
         file_put_contents($path, $message . PHP_EOL, is_file($path) ? FILE_APPEND : 0);
